dd16424df9933d4a00d47a2e45bcab0bf419b4e3
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-10-21 21:52:01 +0300

    More tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,7 +4,6 @@
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-
 bool bsp(const Point a, const Point b, const Point c, const Point point);
 
 int main()
@@ -24,6 +23,27 @@ int main()
     assert(!bsp(a, b, c, Point(6.0f, 3.3f))); // To the right
     assert(!bsp(a, b, c, Point(2.0f, 9.0f))); // Above
     assert(!bsp(a, b, c, Point(2.0f, -9.0f))); // Below
+
+    // Swap triangle point order so that `a` is top right
+    Point a2(c), b2(a), c2(b); 
+
+    // Point is on vertex
+    assert(!bsp(a2, b2, c2, a2));
+    assert(!bsp(a2, b2, c2, b2));
+    assert(!bsp(a2, b2, c2, c2));
+
+    // Point inside triangle
+    assert(bsp(a2, b2, c2, Point(1.4f, 3.3f)));
+
+    // Point outside triangle
+    assert(!bsp(a2, b2, c2, Point(1.0f, 3.3f))); // To the left
+    assert(!bsp(a2, b2, c2, Point(6.0f, 3.3f))); // To the right
+    assert(!bsp(a2, b2, c2, Point(2.0f, 9.0f))); // Above
+    assert(!bsp(a2, b2, c2, Point(2.0f, -9.0f))); // Below
+
+    // Each point is the same
+    Point p(0.0f, 0.0f);
+    assert(!bsp(p, p, p, p));
 
     return EXIT_SUCCESS;
 }
