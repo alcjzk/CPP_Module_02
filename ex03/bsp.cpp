1684f6a408c6694a4d663356b280ad8db893353a
Tuomas <tuomas.jaasalo@alceon.fi>
2023-11-24 16:03:04 +0200

    Fixed division by zero in bsp

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,11 +6,14 @@ bool bsp(const Point a, const Point b, const Point c, const Point p)
     Fixed expr2(p.y() - a.y());
     Fixed expr3(c.x() - a.x());
     Fixed expr4(b.y() - a.y());
-    Fixed w1 = (a.x() * expr1 + expr2 * expr3 - p.x() * expr1)
-        / (expr4 * expr3 - (b.x() - a.x()) * expr1);
-    Fixed w2 = (expr2 - w1 * expr4) / expr1;
+    Fixed w1 = (expr4 * expr3 - (b.x() - a.x()) * expr1);
 
-    return (w1 > Fixed(0) && w1 < Fixed(1) &&
-        w2 > Fixed(0) && w2 < Fixed(1) &&
-        w1 + w2 < Fixed(1));
+    if (expr1 == 0.0f || w1 == 0.0f)
+        return false;
+    Fixed w2 = (a.x() * expr1 + expr2 * expr3 - p.x() * expr1) / w1;
+    Fixed w3 = (expr2 - w2 * expr4) / expr1;
+
+    return (w2 > Fixed(0) && w2 < Fixed(1) &&
+        w3 > Fixed(0) && w3 < Fixed(1) &&
+        w2 + w3 < Fixed(1));
 }
